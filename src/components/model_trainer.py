79fe3a339da51cbf15204677ad85463008d7c362
dannynir <nirmal.chand3791@gmail.com>
2025-01-16 20:46:54 -0800

    Hyperparameters tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -36,15 +36,54 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1]
             )
+            # models = {
+            #     "Random Forest":RandomForestRegressor(),
+            #     "Decision Tree":DecisionTreeRegressor(),
+            #     "Gradient Boosting":GradientBoostingRegressor(),
+            #     "Linear Regression":LinearRegression(),
+            #     "K-Neighbors Classifiers":KNeighborsRegressor(),
+            #     "XGBClassifier":XGBRegressor(),
+            #     "CatBoosting Classifier":CatBoostRegressor(verbose=False),
+            #     "AdaBoost Classifier":AdaBoostRegressor(),
+            # }
             models = {
-                "Random Forest":RandomForestRegressor(),
-                "Decision Tree":DecisionTreeRegressor(),
-                "Gradient Boosting":GradientBoostingRegressor(),
-                "Linear Regression":LinearRegression(),
-                "K-Neighbors Classifiers":KNeighborsRegressor(),
-                "XGBClassifier":XGBRegressor(),
-                "CatBoosting Classifier":CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier":AdaBoostRegressor(),
+                "Random Forest": {
+                    "model_func":RandomForestRegressor(),
+                    "params":{
+                        # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                        # 'max_features':['sqrt','log2',None],
+                        # 'n_estimators': [8,16,32,64,128,256]
+                        'n_estimators': [8,16]
+                    }
+                },
+                "Decision Tree": {
+                    "model_func":DecisionTreeRegressor(),
+                    "params":{
+                        'criterion':['squared_error', 'friedman_mse']
+                        #'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                        # 'splitter':['best','random'],
+                        # 'max_features':['sqrt','log2'],
+                    }
+                },
+                "Gradient Boosting": {
+                    "model_func":GradientBoostingRegressor(),
+                    "params":{
+                        # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                        'learning_rate':[.1,.05],
+                        'subsample':[0.6,0.9],
+                        # 'criterion':['squared_error', 'friedman_mse'],
+                        # 'max_features':['auto','sqrt','log2'],
+                        'n_estimators': [8,32]
+                        }
+                },                
+                "Linear Regression": {
+                    "model_func":LinearRegression(),
+                    "params":{}
+                }
+                # "K-Neighbors Classifiers":KNeighborsRegressor(),
+                # "XGBClassifier":XGBRegressor(),
+                # "CatBoosting Classifier":CatBoostRegressor(verbose=False),
+                # "AdaBoost Classifier":AdaBoostRegressor(),
             }
 
             model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
@@ -52,11 +91,11 @@ class ModelTrainer:
             best_model_score=max(model_report.values())
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
 
-            best_model=models[best_model_name]
+            best_model=models[best_model_name]["model_func"]
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info("Best model found on training and test datasets")
+            logging.info("Best model {0} found on training and test datasets".format(best_model_name))
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -68,4 +107,5 @@ class ModelTrainer:
             return best_model_score
         
         except Exception as e:
+            logging.info(CustomException(e,sys))
             raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -25,14 +26,22 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
         for model in models:
-            models[model].fit(X_train,y_train)
-            y_train_pred = models[model].predict(X_train)
-            y_test_pred = models[model].predict(X_test)
+            model_name = models[model]['model_func']
+            params = models[model]['params']
+            gs=GridSearchCV(model_name,params,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model_name.set_params(**gs.best_params_)
+            model_name.fit(X_train,y_train)
+
+            y_train_pred = model_name.predict(X_train)
+            y_test_pred = model_name.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model]=test_model_score
+            logging.info("Model name {0} Model Params {1}".format(model_name,params))
         return report
 
     except Exception as e:
